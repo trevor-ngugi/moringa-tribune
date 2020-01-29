9f9a8d3621676aa6792460c14f2c059304ff3120
trevor ngugi <ngugitrevor@gmail.com>
2020-01-29 12:45:12 +0300

    add feature to display one article

--- news/views.py
+++ news/views.py
@@ -56,4 +56,11 @@ def search_results(request):
         message = "You haven't searched for any term"
         return render(request, 'all-news/search.html',{"message":message})
 
+def article(request,article_id):
+    try:
+        article = Article.objects.get(id = article_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"all-news/article.html", {"article":article})
+
 
