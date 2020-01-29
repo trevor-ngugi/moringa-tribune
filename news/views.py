e86f2a58c01c62fca895ffbdc59e42a7499e0ca0
trevor ngugi <ngugitrevor@gmail.com>
2020-01-29 11:12:34 +0300

    changed url from news  of today to news today

--- news/views.py
+++ news/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
 import datetime as dt
+from .models import Article
 
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
 
-def news_of_day(request):
-    date = dt.date.today()
-    return render(request, 'all-news/today-news.html', {"date": date,})
+# def news_of_day(request):
+#     date = dt.date.today()
+#     return render(request, 'all-news/today-news.html', {"date": date,})
 
 
 def convert_dates(dates):
@@ -33,8 +34,13 @@ def past_days_news(request,past_date):
         raise Http404()
 
     if date == dt.date.today():
-        return redirect(news_of_day)
-        
-    return render(request, 'all-news/past-news.html', {"date": date})
+        return redirect(news_today)
+    news = Article.days_news(date)
+    return render(request, 'all-news/past-news.html', {"date": date,"news":news})
+
+def news_today(request):
+    date = dt.date.today()
+    news = Article.todays_news()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news})
 
 
