f59dbdc78a99552a38a4ea4909b3649815da6980
trevor ngugi <ngugitrevor@gmail.com>
2020-01-29 11:43:16 +0300

    add search functionallity

--- news/views.py
+++ news/views.py
@@ -43,4 +43,17 @@ def news_today(request):
     news = Article.todays_news()
     return render(request, 'all-news/today-news.html', {"date": date,"news":news})
 
+def search_results(request):
+
+    if 'article' in request.GET and request.GET["article"]:
+        search_term = request.GET.get("article")
+        searched_articles = Article.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-news/search.html',{"message":message,"articles": searched_articles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-news/search.html',{"message":message})
+
 
