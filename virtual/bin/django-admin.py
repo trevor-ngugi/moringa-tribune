0b75d38ccf32091eb35f18447fcb7a6f9735186e
trevor ngugi <ngugitrevor@gmail.com>
2020-01-30 09:24:58 +0300

    remove virtual

--- virtual/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/trevor/Documents/moringa-projects/core/django/the-moringa-tribune/virtual/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
