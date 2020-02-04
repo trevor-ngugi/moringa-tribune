8b10f522a5360092f72ff3ff950932bcd78a9c75
trevor ngugi <ngugitrevor@gmail.com>
2020-02-04 15:47:01 +0300

    add  subcribers form

--- news/models.py
+++ news/models.py
@@ -48,3 +48,6 @@ class Article(models.Model):
     def search_by_title(cls,search_term):
         news = cls.objects.filter(title__icontains=search_term)
         return news
+
+    def __str__(self):
+        return self.title
--- news/views.py
+++ news/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
 import datetime as dt
 from .models import Article
+from .forms import NewsLetterForm
 
 # Create your views here.
 def welcome(request):
@@ -41,7 +42,13 @@ def past_days_news(request,past_date):
 def news_today(request):
     date = dt.date.today()
     news = Article.todays_news()
-    return render(request, 'all-news/today-news.html', {"date": date,"news":news})
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            print('valid')
+    else:
+        form = NewsLetterForm()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form})
 
 def search_results(request):
 
