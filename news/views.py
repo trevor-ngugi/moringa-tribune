c1d24ca45a8eca20e19a5f0619b4e28c44b67e8a
trevor ngugi <ngugitrevor@gmail.com>
2020-02-05 16:43:29 +0300

    add authenification feature

--- news/views.py
+++ news/views.py
@@ -4,6 +4,7 @@ import datetime as dt
 from .models import Article,NewsLetterRecipients
 from .forms import NewsLetterForm
 from .email import send_welcome_email
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def welcome(request):
@@ -69,6 +70,8 @@ def search_results(request):
         message = "You haven't searched for any term"
         return render(request, 'all-news/search.html',{"message":message})
 
+
+@login_required(login_url='/accounts/login/')
 def article(request,article_id):
     try:
         article = Article.objects.get(id = article_id)
